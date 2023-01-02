19fe3f25e65eb4cf00b0aa4146a8fe08093b910a
Shru-04 <shrujanraghu@gmail.com>
2023-01-02 23:31:59 +0530

    Added backend

---
+++ backend/QA-model/app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline, AutoModelForSeq2SeqLM, AutoModelWithLMHead
+import os
+
+app = Flask(__name__)
+
+def modelInit():
+    model_name = "deepset/roberta-base-squad2"
+    return pipeline('question-answering', model=model_name, tokenizer=model_name)
+
+@app.route("/")
+def hello():
+    return "<p>Hello, World!</p>"
+
+@app.route("/run",  methods=['GET', 'POST'])
+def run_model():
+    if request.method == 'POST':
+        try:
+            question = request.args.get('question')
+            unit = int(request.args.get('unit'))
+
+            if (question is '' or question is None):
+                app.aborter(400) 
+        except Exception:
+            app.aborter(400)
+
+        nlp = modelInit()
+        # QA_input = {
+        #                 'question': question,
+        #                 'context': context
+        #             }
+        # resq = nlp(QA_input)
+        context = ''
+        # Set context
+        if unit == 1:
+            context = open(os.getcwd() + '/unit1.txt','r')
+        elif unit == 2:
+            context = open(os.getcwd() + '/unit2.txt','r')
+        elif unit == 3:
+            context = open(os.getcwd() + '/unit3.txt','r')
+        elif unit == 4:
+            context = open(os.getcwd() + '/unit4.txt','r')
+        elif unit == 5:
+            context = open(os.getcwd() + '/unit5.txt','r')
+        else:
+            app.aborter(400)
+        
+        try:
+            context = context.read()
+            QA_input = {
+                            'question': question,
+                            'context': context
+            }
+            resq = nlp(QA_input)
+            return resq
+        except Exception:
+            app.aborter(500)
+    else:
+        app.aborter(405)
